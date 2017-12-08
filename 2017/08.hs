3677d211647accc5169154f5988f41f4317f4ed6
Neil Shepperd <nshepperd@gmail.com>
2017-12-08 00:26:45 -0500

    08

---
+++ 2017/08.hs
@@ -0,0 +1,53 @@
+#!/usr/bin/runhaskell
+{-# LANGUAGE BangPatterns #-}
+
+import           Control.Monad
+import           Data.Foldable
+import           Data.IntMap (IntMap)
+import qualified Data.IntMap as IM
+import           Data.List
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import           Data.Maybe
+import           Data.Ord
+import           Data.Set (Set)
+import qualified Data.Set as Set
+-- import qualified Data.Vector.Generic as V
+-- import qualified Data.Vector.Unboxed as U
+
+type Memory = Map String Int
+type Instruction = Memory -> Memory
+
+parseCondition :: String -> String -> String -> (Memory -> Bool)
+parseCondition creg cmps cvals mem = Map.findWithDefault 0 creg mem `cmp` cval
+  where
+    cmp = case cmps of
+            "<" -> (<)
+            ">" -> (>)
+            "<=" -> (<=)
+            ">=" -> (>=)
+            "==" -> (==)
+            "!=" -> (/=)
+    cval = read cvals
+
+parseLine :: String -> Instruction
+parseLine line = \mem -> if cond mem then
+                           let current = Map.findWithDefault 0 target mem
+                           in Map.insert target (modify current) mem
+                         else mem
+  where
+    [target, act, amounts, "if", creg, cmp, cval] = words line
+    cond = parseCondition creg cmp cval
+    amount = read amounts
+    modify = case act of
+      "inc" -> (+amount)
+      "dec" -> (subtract amount)
+
+parseText :: String -> [Instruction]
+parseText text = map parseLine (lines text)
+
+main :: IO ()
+main = do
+  ins <- parseText <$> readFile "input/08"
+  print $ maximum $ foldr (\f fs -> fs . f) id ins Map.empty
+  print $ maximum $ map maximum $ tail $ scanl (\m f -> f m) Map.empty ins
