8307f275d5c53b9f099753c2e049ed0ebf272387
Neil Shepperd <nshepperd@gmail.com>
2019-12-16 17:47:25 +1100

    cleanup

--- 2019/16.hs
+++ 2019/16.hs
@@ -25,29 +25,27 @@ import           Util
 
 type Vector = U.Vector
 
+-- Precompute subarray sums
 sums :: Vector Int -> Int -> Int -> Int
 sums xs = let ss = V.cons 0 (V.scanl1 (+) xs)
-          in \a b -> sumOf a b ss
-
-sumOf :: Int -> Int -> Vector Int -> Int
-sumOf a b ss = ss V.! (min b (V.length ss - 1)) - ss V.! (min a (V.length ss - 1))
+              sumOf a b = ss V.! (min b (V.length ss - 1)) - ss V.! (min a (V.length ss - 1))
+          in sumOf
 
 fft :: Vector Int -> Vector Int
 fft xs = trace (show (V.take 8 xs)) $ V.generate (V.length xs) go
   where
-    ss = sums xs
+    sumOf = sums xs
     -- go i = let mask = drop 1 $ cycle $ foldMap (replicate (i+1)) [0, 1, 0, -1]
     --        in abs (sum (zipWith (*) (V.toList xs) mask)) `mod` 10
     go i = let c = i + 1
-               total = sum [ ss (4*c*j + 1 * c - 1) (4*c*j + 2 * c - 1)
+               total = sum [ sumOf (4*c*j + 1 * c - 1) (4*c*j + 2 * c - 1)
                            | j <- [0..(V.length xs `div` (4*c))+1]]
-                       - sum [ ss (4*c*j + 3 * c - 1) (4*c*j + 4 * c - 1)
+                       - sum [ sumOf (4*c*j + 3 * c - 1) (4*c*j + 4 * c - 1)
                              | j <- [0..(V.length xs `div` (4*c))+1]]
            in abs total `mod` 10
 
 input :: Integer
 input = 59775675999083203307460316227239534744196788252810996056267313158415747954523514450220630777434694464147859581700598049220155996171361500188470573584309935232530483361639265796594588423475377664322506657596419440442622029687655170723364080344399753761821561397734310612361082481766777063437812858875338922334089288117184890884363091417446200960308625363997089394409607215164553325263177638484872071167142885096660905078567883997320316971939560903959842723210017598426984179521683810628956529638813221927079630736290924180307474765551066444888559156901159193212333302170502387548724998221103376187508278234838899434485116047387731626309521488967864391
-
 
 solve1 :: Integer -> String
 solve1 n = foldMap show $ V.toList $ V.take 8 (iterate fft dat !! 100)
