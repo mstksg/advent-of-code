1f9e25c1794163069a86a51c9f428bf5f2584ab7
Neil Shepperd <nshepperd@gmail.com>
2019-12-21 23:50:25 +1100

    cleanup

--- 2019/21.hs
+++ 2019/21.hs
@@ -11,19 +11,9 @@ import qualified Criterion.Main as Cr
 import           Data.Bits
 import           Data.Char
 import           Data.Foldable
-import           Data.IORef
-import           Data.IntMap (IntMap)
-import qualified Data.IntMap.Strict as IntMap
 import           Data.List
 import           Data.List.Split
-import           Data.Map (Map)
-import qualified Data.Map.Strict as Map
-import           Data.Set (Set)
-import qualified Data.Set as Set
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
 import           Debug.Trace
-import           Linear.V2
 import           System.IO.Unsafe
 
 import           AStar
@@ -36,65 +26,33 @@ data Reg = A | B | C | D | E | F | G | H | I | T | J
 data Inst = And Reg Reg | Or Reg Reg | Not Reg Reg
   deriving (Show, Read, Eq, Ord)
 
-disp :: Inst -> String
-disp (And x y) = "AND " <> show x <> " " <> show y
-disp (Or x y) = "OR " <> show x <> " " <> show y
-disp (Not x y) = "NOT " <> show x <> " " <> show y
+-- Format for the intcode machine
+format :: Inst -> String
+format (And x y) = "AND " <> show x <> " " <> show y
+format (Or x y) = "OR " <> show x <> " " <> show y
+format (Not x y) = "NOT " <> show x <> " " <> show y
 
 prog :: IntCode
 prog = unsafePerformIO $ makeProg . map read . splitOn "," . strip <$> readFile "input/21.txt"
 
--- Tabulation of program function
-
-type Set512 = Integer
-
-full :: Set512
-full = sum [2^i | i <- cs]
-cs = [0..511] :: [Int]
-compl x = full `xor` x
-
-regA = sum [2^i | i <- cs, (.&.) i (2^0) > 0]
-regB = sum [2^i | i <- cs, (.&.) i (2^1) > 0]
-regC = sum [2^i | i <- cs, (.&.) i (2^2) > 0]
-regD = sum [2^i | i <- cs, (.&.) i (2^3) > 0]
-regE = sum [2^i | i <- cs, (.&.) i (2^4) > 0]
-regF = sum [2^i | i <- cs, (.&.) i (2^5) > 0]
-regG = sum [2^i | i <- cs, (.&.) i (2^6) > 0]
-regH = sum [2^i | i <- cs, (.&.) i (2^7) > 0]
-regI = sum [2^i | i <- cs, (.&.) i (2^8) > 0]
-
-simulate :: [Inst] -> (Set512, Set512)
-simulate xs = simulateStep xs (0, 0)
-simulateStep :: [Inst] -> (Set512, Set512) -> (Set512, Set512)
-simulateStep xs start = go start xs
+-- Simulate the jump decision on a given piece of terrain.
+simulate :: [Inst] -> String -> Int
+simulate ops terrain = go ops 0 0
   where
-    go (t, j) [] = (t, j)
-    go (t, j) (x:xs) = case x of
-      (And a J) -> (t, reg a .&. j)
-      (And a T) -> (reg a .&. t, j)
-      (Or a J) -> (t, reg a .|. j)
-      (Or a T) -> (reg a .|. t, j)
-      (Not a J) -> (t, compl (reg a))
-      (Not a T) -> (compl (reg a), j)
+    go [] t j = j
+    go (op:ops) t j = case op of
+      And r T -> go ops (reg r .&. t) j
+      And r J -> go ops t (reg r .&. j)
+      Or r T -> go ops (reg r .|. t) j
+      Or r J -> go ops t (reg r .|. j)
+      Not r T -> go ops (1 - reg r) j
+      Not r J -> go ops t (1 - reg r)
       where
-        reg :: Reg -> Set512
-        reg A = regA
-        reg B = regB
-        reg C = regC
-        reg D = regD
-        reg E = regE
-        reg F = regF
-        reg G = regG
-        reg H = regH
-        reg I = regI
         reg T = t
         reg J = j
-
-simOn :: Set512 -> String -> Int
-simOn vset cs = if vset .&. (2^i) > 0 then 1 else 0
-  where
-    bs = [if c == '#' then 1 else 0 | c <- cs] :: [Int]
-    i = sum [b * bit i | (i, b) <- zip [0..8] bs]
+        reg r = case terrain !! (fromEnum r) of
+                  '#' -> 1
+                  '.' -> 0
 
 -- Solutions
 
@@ -103,29 +61,18 @@ display output
   | last output > 256 = print (last output)
   | otherwise = putStr (map chr output)
 
--- solve1 :: IO ()
--- solve1 = print $ last $ runProg prog $ map ord $ unlines (map disp p <> ["WALK"])
---   where
---     p = [Not A J,
---          Not B T,
---          Or T J,
---          And D J,
---          Not C T,
---          And A T,
---          And B T,
---          And D T,
---          Or T J
---         ]
+execute :: [Inst] -> String -> [Int]
+execute p level = runProg prog $ map ord $ unlines (map format p <> [level])
 
+-- [Or A J,And C J,And B J,Not J J,And D J]
 solve1 :: IO ()
-solve1 = let solution = bfsOn viewCases actions goal ([], simulate []) :: ([Inst], (Set512, Set512))
-         in display (execute solution)
+solve1 = let solution = bfsOn viewCases actions goal [] :: [Inst]
+         in print solution >> display (execute solution "WALK")
   where
-    actions (p, sim) = do f <- [And, Or, Not]
-                          r1 <- [A .. D] ++ [T, J]
-                          r2 <- [T, J]
-                          return (p ++ [f r1 r2], simulateStep [f r1 r2] sim)
-    execute (p, sim) = runProg prog $ map ord $ unlines (map disp p <> ["WALK"])
+    actions p = do f <- [And, Or, Not]
+                   r1 <- [A .. D] ++ [T, J]
+                   r2 <- [T, J]
+                   return (p ++ [f r1 r2])
 
     doJump = [
       ".########",
@@ -138,20 +85,20 @@ solve1 = let solution = bfsOn viewCases actions goal ([], simulate []) :: ([Inst
       "#...#####"
       ]
 
-    viewCases (p, (t, j)) = [simOn j cs | cs <- doJump ++ noJump]
+    viewCases p = [simulate p cs | cs <- doJump ++ noJump]
 
-    goal (p, (t, j)) = and [simOn j cs == 1 | cs <- doJump]
-                       && and [simOn j cs == 0 | cs <- noJump]
+    goal p = and [simulate p cs == 1 | cs <- doJump]
+             && and [simulate p cs == 0 | cs <- noJump]
 
+-- [Not H J,Or C J,And B J,And A J,Not J J,And D J]
 solve2 :: IO ()
-solve2 = let solution = bfsOn viewCases actions goal ([], simulate []) :: ([Inst], (Set512, Set512))
-         in display (execute solution)
+solve2 = let solution = bfsOn viewCases actions goal [] :: [Inst]
+         in print solution >> display (execute solution "RUN")
   where
-    actions (p, sim) = do f <- [And, Or, Not]
-                          r1 <- [A .. J]
-                          r2 <- [T, J]
-                          return (p ++ [f r1 r2], simulateStep [f r1 r2] sim)
-    execute (p, sim) = runProg prog $ map ord $ unlines (map disp p <> ["RUN"])
+    actions p = do f <- [And, Or, Not]
+                   r1 <- [A .. J]
+                   r2 <- [T, J]
+                   return (p ++ [f r1 r2])
 
     doJump = [
       ".########",
@@ -177,12 +124,26 @@ solve2 = let solution = bfsOn viewCases actions goal ([], simulate []) :: ([Inst
       "####.####"
       ]
 
-    viewCases (p, (t, j)) = [simOn j cs | cs <- doJump ++ noJump]
+    viewCases p = [simulate p cs | cs <- doJump ++ noJump]
 
-    goal (p, (t, j)) = and [simOn j cs == 1 | cs <- doJump]
-                       && and [simOn j cs == 0 | cs <- noJump]
+    goal p = and [simulate p cs == 1 | cs <- doJump]
+             && and [simulate p cs == 0 | cs <- noJump]
 
 main :: IO ()
 main = do
   solve1
   solve2
+  -- Cr.defaultMain [
+  --   Cr.bench "solve1" (Cr.nfIO solve1),
+  --   Cr.bench "solve2" (Cr.nfIO solve2)
+  --   ]
+
+-- time                 9.754 ms   (9.540 ms .. 9.937 ms)
+--                      0.998 R²   (0.997 R² .. 0.999 R²)
+-- mean                 9.563 ms   (9.499 ms .. 9.645 ms)
+-- std dev              187.6 μs   (132.2 μs .. 254.9 μs)
+
+-- time                 172.8 ms   (170.1 ms .. 175.0 ms)
+--                      1.000 R²   (1.000 R² .. 1.000 R²)
+-- mean                 174.1 ms   (173.1 ms .. 174.5 ms)
+-- std dev              983.9 μs   (389.3 μs .. 1.521 ms)
