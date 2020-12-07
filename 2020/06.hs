8932f086d5fd54f9583c683bf6c5c3072046b190
nshepperd <nshepperd@gmail.com>
2020-12-07 15:09:10 +1100

    Day 6

---
+++ 2020/06.hs
@@ -0,0 +1,50 @@
+{-# Language ConstraintKinds #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import           Control.Applicative
+import           Control.Concurrent
+import           Control.Concurrent.STM
+import           Control.Concurrent.STM.TQueue
+import           Control.Monad
+import           Data.Bits
+import           Data.Char
+import           Data.Foldable
+import           Data.Function.Memoize
+import           Data.Int
+import           Data.List
+import           Data.Map (Map)
+import qualified Data.Map.Strict as Map
+import           Data.Set (Set)
+import qualified Data.Set as Set
+import           Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import           Data.Traversable
+import           Data.Vector (Vector)
+import qualified Data.Vector.Generic as V
+import qualified Data.Vector.Unboxed as U
+import           Data.Word
+import           Debug.Trace
+import           System.IO
+import           System.IO.Unsafe
+import           Text.Parser.Char
+import           Text.Parser.Combinators hiding (count)
+
+import           Util
+import qualified Util.Text as T
+
+input :: [[Text]]
+input = unsafePerformIO (process <$> T.readFile "input/06.txt")
+  where
+    process = map T.lines . T.splitOn "\n\n"
+
+part1 = sum [Set.size $ Set.fromList (fold [T.unpack l | l <- group]) | group <- input]
+
+part2 = sum [Set.size $ foldr1 Set.intersection ([Set.fromList (T.unpack l) | l <- group])
+            | group <- input]
