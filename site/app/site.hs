0b4c05724bb78902c8fae134633121c1c8cd3729
justin <justin@jle.im>
2023-12-27 05:24:39 +0000

    start building hakyll

--- site/app/site.hs
+++ site/app/site.hs
@@ -1,3 +1,16 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import Hakyll
+
+config :: Configuration
+config =
+  defaultConfiguration
+    { providerDirectory = "pages"
+    }
 
 main :: IO ()
-main = putStrLn "hello"
+main = hakyllWith config do
+  match "reflections/*/*" do
+    route $ setExtension "html"
+    compile pandocCompiler
