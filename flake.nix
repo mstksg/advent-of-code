d35037f0af5a705b86eab054307dfaf954ae2368
justin <justin@jle.im>
2024-10-25 00:54:02 -0700

    new flake and envrc

--- flake.nix
+++ flake.nix
@@ -8,49 +8,51 @@
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [
-          haskellNix.overlay
-          (final: prev: {
-            advent-of-code = final.haskell-nix.project' {
-              name = "advent-of-code";
-              src = ./.;
-              compiler-nix-name = "ghc964";
-              shell = {
-                withHoogle = false;
-                tools = {
-                  cabal = { };
-                  hlint = { };
-                  haskell-language-server = { };
-                  fourmolu = { };
-                };
-              };
-            };
-          })
-        ];
+        overlays = [ haskellNix.overlay ];
         pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-        flake = pkgs.advent-of-code.flake { };
-      in
-      flake
-      // {
-        packages =
-          pkgs.advent-of-code.hsPkgs.advent-of-code.components.exes
-          //
-          {
-            default = flake.packages."advent-of-code:exe:aoc2023";
+        advent-of-code-project = pkgs.haskell-nix.project' {
+          name = "advent-of-code";
+          src = ./.;
+          compiler-nix-name = "ghc982";
+          shell = {
+            withHoogle = false;
+            buildInputs = [ format-haskell ];
+            tools = {
+              cabal = { };
+              hlint = { };
+              haskell-language-server = { };
+              fourmolu = { };
+            };
           };
-        legacyPackages = pkgs;
-        apps.format = {
-          type = "app";
-          program =
-            toString
-              (pkgs.writeShellApplication {
-                name = "formatHaskell.sh";
-                runtimeInputs = [ flake.devShell ];
-                text = ''
-                  # shellcheck disable=SC2046
-                  fourmolu --mode inplace $(git ls-files '*.hs')
-                '';
-              }) + "/bin/formatHaskell.sh";
+        };
+        inherit (advent-of-code-project.hsPkgs) advent-of-code;
+        format-haskell =
+          pkgs.writeShellApplication {
+            name = "format-haskell";
+            runtimeInputs = [ (advent-of-code-project.tool "fourmolu" { }) pkgs.haskellPackages.cabal-fmt ];
+            text = ''
+              # shellcheck disable=SC2046
+              fourmolu --mode inplace $(git ls-files 'haskell/*.hs')
+              cabal-fmt -i advent-of-code.cabal
+            '';
+          };
+      in
+      {
+        packages = advent-of-code.components.exes //
+          { default = advent-of-code.components.exes.aoc2023; };
+        devShells.default = advent-of-code-project.shell;
+        legacyPackages = pkgs // { inherit advent-of-code-project; };
+        apps = {
+          format =
+            pkgs.writeShellApplication {
+              name = "format-haskell";
+              runtimeInputs = [ (advent-of-code-project.tool "fourmolu" { }) pkgs.haskellPackages.cabal-fmt ];
+              text = ''
+                # shellcheck disable=SC2046
+                fourmolu --mode inplace $(git ls-files '**.hs')
+                cabal-fmt -i advent-of-code.cabal
+              '';
+            };
         };
       }
     );
