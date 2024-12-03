780b41b7b1f6853dcc8c5e565606727098d8cd66
justin <justin@jle.im>
2024-12-02 21:13:02 -0800

    initial day 3

--- 2024/AOC2024/Day03.hs
+++ 2024/AOC2024/Day03.hs
@@ -20,11 +20,10 @@
 --     types @_ :~> _@ with the actual types of inputs and outputs of the
 --     solution.  You can delete the type signatures completely and GHC
 --     will recommend what should go in place of the underscores.
-module AOC2024.Day03
-  ( 
-    -- day03a,
-    -- day03b
-  )
+module AOC2024.Day03 (
+  day03a,
+  day03b,
+)
 where
 
 import AOC.Prelude
@@ -50,25 +49,43 @@ import qualified Text.Megaparsec as P
 import qualified Text.Megaparsec.Char as P
 import qualified Text.Megaparsec.Char.Lexer as PP
 
-day03a :: _ :~> _
+parseMul :: CharParser Int
+parseMul = do 
+  "mul("
+  x <- pDecimal
+  ","
+  y <- pDecimal
+  ")"
+  pure (x*y)
+
+day03a :: [Int] :~> _
 day03a =
   MkSol
-    { sParse =
+    { sParse = Just . mapMaybe (parseMaybe' parseMul) . tails
+    , sShow = show
+    , sSolve =
         noFail $
-          lines
-    ,
-      sShow = show,
-      sSolve =
-        noFail $
-          id
+          sum
     }
+
+-- secMay f (x, y) = (x,) <$> f y
 
 day03b :: _ :~> _
 day03b =
   MkSol
-    { sParse = sParse day03a,
-      sShow = show,
-      sSolve =
-        noFail $
-          id
+    { sParse = \xs -> Just $ 
+        let muls = mapMaybe (traverse $ parseMaybe' parseMul) . zip [0..] . tails $ xs
+            dos = map fst . filter (("do()" `isPrefixOf`) . snd) . zip [0..] . tails $ xs
+            donts = map fst . filter (("don't()" `isPrefixOf`) . snd) . zip [0..] . tails $ xs
+         in (muls, dos, donts)
+    , sShow = show
+    , sSolve = noFail \(muls, dos, donts) ->
+      let dodontMap = M.fromSet (const True) (S.fromList dos) <> M.fromSet (const False) (S.fromList donts)
+          good i = case M.lookupLE i dodontMap of
+                      Nothing -> False
+                      Just (_, t) -> t
+       in sum . map snd $ filter (good . fst) muls
+        
+        -- noFail $
+        --   id
     }
