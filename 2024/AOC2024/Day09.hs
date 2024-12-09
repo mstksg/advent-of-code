4ceb5e6f3201ee5ad526478a5db641dfa9076245
justin <justin@jle.im>
2024-12-08 22:30:04 -0800

    initial day 9

--- 2024/AOC2024/Day09.hs
+++ 2024/AOC2024/Day09.hs
@@ -21,9 +21,8 @@
 --     solution.  You can delete the type signatures completely and GHC
 --     will recommend what should go in place of the underscores.
 module AOC2024.Day09 (
--- day09a,
--- day09b
-
+  day09a,
+  day09b,
 )
 where
 
@@ -55,11 +54,21 @@ day09a =
   MkSol
     { sParse =
         noFail $
-          lines
+          map digitToInt
     , sShow = show
     , sSolve =
-        noFail $
-          id
+        noFail \ls ->
+          let board =
+                M.fromList . mapMaybe (\(i, x) -> (i :: Int,) <$> x) . zip [0 ..] . concat $
+                  zipWith replicate ls (intersperse Nothing $ Just <$> [0 ..])
+              maxIx = last $ M.keys board
+              allIx = [0 .. maxIx]
+              (finalBoard, _) = flip loopMaybe (board, 0) \(ps, n) -> traceShow n do
+                let (iLast, pLast) = M.findMax ps
+                firstGap <- find (`M.notMember` ps) allIx
+                guard $ firstGap /= (iLast + 1)
+                pure (M.insert firstGap pLast $ M.delete iLast ps, n + 1)
+           in sum . map (uncurry (*)) $ M.toList finalBoard
     }
 
 day09b :: _ :~> _
@@ -68,6 +77,28 @@ day09b =
     { sParse = sParse day09a
     , sShow = show
     , sSolve =
-        noFail $
-          id
+        noFail \ls ->
+          let board = M.fromList $
+                flip evalState 0 $
+                  for (zip ls (intersperse Nothing $ Just <$> [0 ..])) \(len, fid) -> state \i ->
+                    ((i, (fid, len)), i + len)
+              emptyBoard :: M.Map Int Int
+              emptyBoard = M.mapMaybe (\(fid, i) -> case fid of Nothing -> Just i; Just _ -> Nothing) board
+              fullBoard :: M.Map Int (Int, Int)
+              fullBoard = M.mapMaybe (\(fid, i) -> (,i) <$> fid) board
+              (_, _, finalFull) = flip loopMaybe (S.empty, emptyBoard, fullBoard) \(moved, psEmpty, psFull) -> do
+                (gapI, gapLen, moveMeI, moveMe, moveMeLen) <- flip firstJust (M.toDescList psFull) \(i, (fid, len)) -> do
+                  guard $ fid `S.notMember` moved
+                  (gapI, gapLen) <- find ((len <=) . snd) (M.toAscList psEmpty)
+                  guard $ gapI < i
+                  pure (gapI, gapLen, i, fid, len)
+                let reAddEmpty
+                      | gapLen == moveMeLen = id
+                      | otherwise = M.insert (gapI + moveMeLen) (gapLen - moveMeLen)
+                    newEmpty = reAddEmpty $ M.delete gapI psEmpty
+                    newFull = M.insert gapI (moveMe, moveMeLen) . M.delete moveMeI $ psFull
+                    newMoved = S.insert moveMe moved
+                pure (newMoved, newEmpty, newFull)
+              checksum (i, (v, len)) = sum . map (* v) . take len $ [i ..]
+           in sum . map checksum $ M.toList finalFull
     }
