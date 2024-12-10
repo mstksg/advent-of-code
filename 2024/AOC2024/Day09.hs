e611827eca6ee8db377a86a2232d512fa6eeb3dc
justin <justin@jle.im>
2024-12-10 00:26:27 -0800

    clean up day 9 even more

--- 2024/AOC2024/Day09.hs
+++ 2024/AOC2024/Day09.hs
@@ -14,13 +14,12 @@ where
 
 import AOC.Solver (noFail, type (:~>) (..))
 import Control.DeepSeq (NFData)
-import Control.Monad.Trans.State (evalState, runState, state)
+import Control.Monad.State (MonadState (state), evalState)
 import Data.Char (digitToInt)
-import Data.Foldable (Foldable (foldl'), find)
+import Data.Foldable (find)
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IM
 import Data.List (intersperse)
-import Data.Traversable (for)
 import GHC.Generics (Generic, Generically (..))
 
 data DiskState a = DS
@@ -45,6 +44,19 @@ toDiskState fids =
       Nothing -> Left len
       Just fid -> Right (fid, len)
 
+fillGaps :: [(Int, Int)] -> [(Int, Int)] -> Int
+fillGaps = \case
+  [] -> sum . map (uncurry (*))
+  (gapI, gapLen) : gaps -> \case
+    [] -> 0
+    (endI, fid) : ends
+      | endI > gapI ->
+          let addBack
+                | gapLen == 1 = id
+                | otherwise = ((gapI + 1, gapLen - 1) :)
+           in gapI * fid + fillGaps (addBack gaps) ends
+      | otherwise -> endI * fid + sum (map (uncurry (*)) ends)
+
 day09a :: [Int] :~> Int
 day09a =
   MkSol
@@ -61,29 +73,21 @@ day09a =
                 | (i0, (fid, len)) <- IM.toDescList dsFiles
                 , i <- take len $ iterate (subtract 1) (i0 + len - 1)
                 ]
-              (newGaps, newFiles) = flip runState endFiles . fmap concat . for (IM.toAscList dsGaps) $ \(i0, gapLen) -> state \ends ->
-                let (fillMe, rest) = splitAt gapLen ends
-                    (fillToTheMiddle, pastMiddle) = span (\((endI, _), gapI) -> endI > gapI) $ zip fillMe [i0 ..]
-                 in ((\((_, fid), i) -> (i, fid)) <$> fillToTheMiddle, (fst <$> pastMiddle) ++ rest)
-           in sum . map (uncurry (*)) $ newGaps ++ newFiles
+           in fillGaps (IM.toAscList dsGaps) endFiles
     }
 
-advanceBlock :: DiskState a -> (Int, (a, Int)) -> DiskState a
-advanceBlock ds (i, (fid, fileLen)) =
-  maybe ds update . find ((>= fileLen) . snd) . IM.toAscList . IM.takeWhileAntitone (< i) $ dsGaps ds
-  where
-    update (gapI, gapLen) =
-      DS
-        { dsGaps = IM.union reGap . IM.delete gapI $ dsGaps ds
-        , dsFiles = IM.insert gapI (fid, fileLen) . IM.delete i $ dsFiles ds
-        }
-      where
-        reGap = IM.filter (> 0) $ IM.singleton (gapI + fileLen) (gapLen - fileLen)
-
-checksum :: IM.IntMap (Int, Int) -> Int
-checksum = sum . map go . IM.toList
-  where
-    go (i, (v, len)) = sum . map (* v) . take len $ [i ..]
+moveBlocks :: IntMap Int -> [(Int, (Int, Int))] -> Int
+moveBlocks gaps = \case
+  [] -> 0
+  (i, (fid, fileLen)) : rest ->
+    let foundGap = find ((>= fileLen) . snd) . IM.toAscList $ IM.takeWhileAntitone (< i) gaps
+        hereContrib = sum . map (* fid) . take fileLen $ [maybe i fst foundGap ..]
+        gaps' = case foundGap of
+          Nothing -> gaps
+          Just (gapI, gapLen) ->
+            let reGap = IM.filter (> 0) $ IM.singleton (gapI + fileLen) (gapLen - fileLen)
+             in IM.union reGap . IM.delete gapI $ gaps
+     in hereContrib + moveBlocks gaps' rest
 
 day09b :: [Int] :~> Int
 day09b =
@@ -92,6 +96,6 @@ day09b =
     , sShow = show
     , sSolve =
         noFail \ls ->
-          let ds0 = toDiskState [0 ..] ls
-           in checksum . dsFiles . foldl' advanceBlock ds0 $ IM.toDescList (dsFiles ds0)
+          let DS{..} = toDiskState [0 ..] ls
+           in moveBlocks dsGaps $ IM.toDescList dsFiles
     }
