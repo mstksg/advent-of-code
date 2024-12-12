12bf49043aac40f8b0d55b7fc2bc46b92f6f70f7
justin <justin@jle.im>
2024-12-11 22:47:04 -0800

    cleann up day 12

--- 2024/AOC2024/Day12.hs
+++ 2024/AOC2024/Day12.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC2024.Day12
 -- License     : BSD3
@@ -9,85 +6,68 @@
 -- Portability : non-portable
 --
 -- Day 12.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 module AOC2024.Day12 (
-day12a,
-day12b
-
+  day12a,
+  day12b,
 )
 where
 
-import AOC.Prelude
-import qualified Data.Graph.Inductive as G
-import qualified Data.IntMap as IM
-import qualified Data.IntMap.NonEmpty as NEIM
-import qualified Data.IntSet as IS
-import qualified Data.IntSet.NonEmpty as NEIS
-import qualified Data.List.NonEmpty as NE
-import qualified Data.List.PointedList as PL
-import qualified Data.List.PointedList.Circular as PLC
+import AOC.Common.Point (Dir (North), Point, contiguousRegions, dirPoint, parseAsciiMap)
+import AOC.Solver (noFail, type (:~>) (..))
+import Data.Foldable (Foldable (toList))
+import Data.Map (Map)
 import qualified Data.Map as M
-import qualified Data.Map.NonEmpty as NEM
-import qualified Data.OrdPSQ as PSQ
-import qualified Data.Sequence as Seq
-import qualified Data.Sequence.NonEmpty as NESeq
+import Data.Set (Set)
 import qualified Data.Set as S
+import Data.Set.NonEmpty (NESet)
 import qualified Data.Set.NonEmpty as NES
-import qualified Data.Text as T
-import qualified Data.Vector as V
-import qualified Linear as L
-import qualified Text.Megaparsec as P
-import qualified Text.Megaparsec.Char as P
-import qualified Text.Megaparsec.Char.Lexer as PP
 
-perimeter :: Set Point -> Int
-perimeter pts = sum
-  [ S.size $ cardinalNeighbsSet p `S.difference` pts
+regionNeighbors :: Set Point -> Map Dir (Set Point)
+regionNeighbors pts =
+  M.fromListWith
+    (<>)
+    [ (d, S.singleton p')
     | p <- toList pts
-  ]
+    , d <- [North ..]
+    , let p' = p + dirPoint d
+    , p' `S.notMember` pts
+    ]
 
-day12a :: _ :~> _
+regions :: Ord a => Map Point a -> Map a (Set (NESet Point))
+regions mp =
+  contiguousRegions
+    <$> M.fromListWith
+      (<>)
+      [ (x, S.singleton p)
+      | (p, x) <- M.toList mp
+      ]
+
+day12a :: Map Point Char :~> Int
 day12a =
   MkSol
     { sParse = noFail $ parseAsciiMap Just
     , sShow = show
     , sSolve =
-        noFail \mp ->
-          let regions = contiguousRegions <$> M.fromListWith (<>)
-                  [ (x, S.singleton p)
-                    | (p, x) <- M.toList mp
-                  ]
-          in sum [ perimeter reg * S.size reg | regs <- toList regions, reg <- NES.toSet <$> toList regs ]
+        noFail $ \mp ->
+          sum
+            [ S.size region * S.size dirRegion
+            | letterRegions <- toList $ regions mp
+            , region <- NES.toSet <$> toList letterRegions
+            , dirRegion <- toList $ regionNeighbors region
+            ]
     }
 
-sides :: Set Point -> Int
-sides pts = sum . map S.size . toList $ contiguousRegions <$> M.fromListWith (<>)
-  [ (d, S.singleton p)
-    | p <- toList pts
-    , d <- [ North .. ]
-  , (p + dirPoint d) `S.notMember` pts
-  ]
-
-day12b :: _ :~> _
+day12b :: Map Point Char :~> Int
 day12b =
   MkSol
     { sParse = sParse day12a
     , sShow = show
     , sSolve =
         noFail \mp ->
-          let regions = contiguousRegions <$> M.fromListWith (<>)
-                  [ (x, S.singleton p)
-                    | (p, x) <- M.toList mp
-                  ]
-          in sum [ sides reg * S.size reg | regs <- toList regions, reg <- NES.toSet <$> toList regs ]
+          sum
+            [ S.size region * S.size (contiguousRegions dirRegion)
+            | letterRegions <- toList $ regions mp
+            , region <- NES.toSet <$> toList letterRegions
+            , dirRegion <- toList $ regionNeighbors region
+            ]
     }
