b1c94b003e74d708a7a141c0b4cbb495d4fd2d56
justin <justin@jle.im>
2024-12-17 01:33:49 -0800

    day 17 in progress

--- 2024/AOC2024/Day17.hs
+++ 2024/AOC2024/Day17.hs
@@ -21,9 +21,8 @@
 --     solution.  You can delete the type signatures completely and GHC
 --     will recommend what should go in place of the underscores.
 module AOC2024.Day17 (
--- day17a,
--- day17b
-
+day17a,
+day17b
 )
 where
 
@@ -35,6 +34,7 @@ import qualified Data.IntSet as IS
 import qualified Data.IntSet.NonEmpty as NEIS
 import qualified Data.List.NonEmpty as NE
 import qualified Data.List.PointedList as PL
+import Data.Bits
 import qualified Data.List.PointedList.Circular as PLC
 import qualified Data.Map as M
 import qualified Data.Map.NonEmpty as NEM
@@ -53,21 +53,163 @@ import qualified Text.Megaparsec.Char.Lexer as PP
 day17a :: _ :~> _
 day17a =
   MkSol
-    { sParse =
-        noFail $
-          lines
-    , sShow = show
+    { sParse = parseMaybe' do
+        a <- "Register A: " *> pDecimal
+        P.newline
+        b <- "Register B: " *> pDecimal
+        P.newline
+        c <- "Register C: " *> pDecimal
+        P.newline
+        P.newline
+        d <- "Program: " *> (pDecimal `sepBy'` ",")
+        pure (a,b,c,d)
+    , sShow = intercalate "," . map show
     , sSolve =
-        noFail $
-          id
+        noFail $ \(a,b,c,p :: [Int]) ->
+              go 0 (V3 a b c) (Seq.fromList p)
     }
+
+go :: Int -> V3 Int -> Seq Int -> [Int]
+go i (V3 a b c) tp = traceShow (i, V3 a b c) $ case (,) <$> Seq.lookup i tp <*> Seq.lookup (i + 1) tp of
+  Nothing -> []
+  Just (q,o) ->
+    let x = case o of
+              0 -> 0
+              1 -> 1
+              2 -> 2
+              3 -> 3
+              4 -> a
+              5 -> b
+              6 -> c
+    in case q of
+         0 -> go (i + 2) (V3 (a `div` (2^x)) b c) tp
+         1 -> go (i + 2) (V3 a (b `xor` o) c) tp
+         2 -> go (i + 2) (V3 a (x `mod` 8) c) tp
+         3 | a == 0 -> go (i + 2) (V3 a b c) tp
+           | otherwise -> go o (V3 a b c) tp
+         4 -> go (i + 2) (V3 a (b `xor` c) c) tp
+         5 -> trace (show (x `mod` 8, o, x)) (x `mod` 8) : go (i + 2) (V3 a b c) tp
+         6 -> go (i + 2) (V3 a (a `div` (2^x)) c) tp
+         7 -> go (i + 2) (V3 a b (a `div` (2^x))) tp
+
+
+-- 2,4, 1,6, 7,5, 4,6, 1,4, 5,5, 0,3, 3,0
+--
+-- BST A    --- b = (a `mod` 8)
+-- BXL 6    --- b ^= 110    (6)
+-- CDV B    --- c = a / (2^b)
+-- BXC      --- b ^= c
+-- BXL 4    --- b ^= 100    (4)
+-- OUT B    --- print b
+-- ADV 3    --- a /= 8
+-- JNZ 0
+
+-- The *`adv`* instruction (opcode *`0`*) performs *division*. The
+-- numerator is the value in the `A` register. The denominator is found by
+-- raising 2 to the power of the instruction's *combo* operand. (So, an
+-- operand of `2` would divide `A` by `4` (`2^2`); an operand of `5` would
+-- divide `A` by `2^B`.) The result of the division operation is
+-- *truncated* to an integer and then written to the `A` register.
+
+-- The *`bxl`* instruction (opcode *`1`*) calculates the [bitwise
+-- XOR](https://en.wikipedia.org/wiki/Bitwise_operation#XOR){target="_blank"}
+-- of register `B` and the instruction's *literal* operand, then stores the
+-- result in register `B`.
+
+-- The *`bst`* instruction (opcode *`2`*) calculates the value of its
+-- *combo* operand
+-- [modulo](https://en.wikipedia.org/wiki/Modulo){target="_blank"} 8
+-- (thereby keeping only its lowest 3 bits), then writes that value to the
+-- `B` register.
+
+-- The *`jnz`* instruction (opcode *`3`*) does *nothing* if the `A`
+-- register is `0`. However, if the `A` register is *not zero*, it
+-- [*jumps*]{title="The instruction does this using a little trampoline."}
+-- by setting the instruction pointer to the value of its *literal*
+-- operand; if this instruction jumps, the instruction pointer is *not*
+-- increased by `2` after this instruction.
+
+-- The *`bxc`* instruction (opcode *`4`*) calculates the *bitwise XOR* of
+-- register `B` and register `C`, then stores the result in register `B`.
+-- (For legacy reasons, this instruction reads an operand but *ignores*
+-- it.)
+
+-- The *`out`* instruction (opcode *`5`*) calculates the value of its
+-- *combo* operand modulo 8, then *outputs* that value. (If a program
+-- outputs multiple values, they are separated by commas.)
+
+-- The *`bdv`* instruction (opcode *`6`*) works exactly like the `adv`
+-- instruction except that the result is stored in the *`B` register*. (The
+-- numerator is still read from the `A` register.)
+
+-- The *`cdv`* instruction (opcode *`7`*) works exactly like the `adv`
+-- instruction except that the result is stored in the *`C` register*. (The
+-- numerator is still read from the `A` register.)
+-- --
 
 day17b :: _ :~> _
 day17b =
   MkSol
     { sParse = sParse day17a
     , sShow = show
+    -- , sShow = intercalate "\n" . map show
     , sSolve =
-        noFail $
-          id
+        noFail $ \(_,_,_,p :: [Int]) ->
+          execState (stepBackwards (reverse p)) (V3 0 0 0)
+          -- [ (go 0 (V3 i b c) (Seq.fromList p))
+          -- -- | i <- [45184372088832]
+          -- | i <- [1999]
+          -- ]
     }
+
+stepBackwards :: [Int] -> State (V3 Int) ()
+stepBackwards = \case
+  [] -> pure ()   -- still need to progress back up to the first
+  o:os -> do
+    -- oh no, this is wrong because we only get o mod 8
+    -- _y .= (o `xor` 6)
+    let lsbA = [ a
+                | a <- [0..1000]
+               , let b0 = (a .&. 7) `xor` 6
+               , (b0 `xor` (a `shift` (-b0))) .&. 7 == o
+              ]
+    traceM $ show (o, lsbA)
+    pure ()
+
+-- CDV B    --- c = a / (2^b)
+-- BXC      --- b ^= c
+--
+-- c = a / (2^b0)
+-- b = b0 ^ c
+--
+-- b = b0 ^ (a >> b0)
+--
+-- b = (b0 ^ 110) ^ (a >> (b0 ^ 110))
+--
+-- b = ((a `mod` 8) ^ 110) ^ (a >> ((a `mod` 8) ^ 110))
+-- 
+-- mod 8 is & 111
+--
+-- b & 111 = ((a & 111) ^ 110) ^ (a >> ((a & 111) ^ 110))
+-- b & 111 = ((a ^ 110) & 111) ^ (a >> ((a ^ 110) & 111))
+--
+-- i guess we can do a search for what (a & 111) is, it's only 7 possible
+-- items. But how do we get from that to the full A?
+--
+-- Ah hah i guess we can store the rest of the A trit-by-trit.
+--
+--
+--
+
+-- stepBackwards r@(V3 a b c) = \case
+--   [] -> r
+--   x:xs ->
+
+-- BST A    --- b = (a `mod` 8)
+-- BXL 6    --- b ^= 110    (6)
+-- CDV B    --- c = a / (2^b)
+-- BXC      --- b ^= c
+-- BXL 4    --- b ^= 100    (4)
+-- OUT B    --- print (b `mod` 8)
+-- ADV 3    --- a /= 8
+-- JNZ 0
