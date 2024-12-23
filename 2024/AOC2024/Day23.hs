78215f94013bbf214ccb8e05e813a3fa4bcfb733
justin <justin@jle.im>
2024-12-23 00:00:34 -0800

    day 23 cleaned up, benches

--- 2024/AOC2024/Day23.hs
+++ 2024/AOC2024/Day23.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC2024.Day23
 -- License     : BSD3
@@ -9,117 +6,74 @@
 -- Portability : non-portable
 --
 -- Day 23.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 module AOC2024.Day23 (
-day23a,
-day23b
+  day23a,
+  day23b,
 )
 where
 
-import AOC.Prelude
-import qualified Data.Graph.Inductive as G
-import qualified Data.IntMap as IM
-import qualified Data.IntMap.NonEmpty as IM
-import qualified Data.Tree as Tree
-import qualified Data.IntSet as IS
-import qualified Data.IntSet.NonEmpty as NEIS
-import qualified Data.List.NonEmpty as NE
-import qualified Data.List.PointedList as PL
-import qualified Data.List.PointedList.Circular as PLC
+import AOC.Common (countTrue)
+import AOC.Common.Parser (pAlphaNumWord, parseMaybe', sepByLines, sequenceSepBy)
+import AOC.Solver (noFail, type (:~>) (..))
+import Control.Monad (guard)
+import Data.Foldable (Foldable (toList))
+import Data.List (intercalate, isPrefixOf, sort)
+import Data.Map (Map)
 import qualified Data.Map as M
-import qualified Data.Map.NonEmpty as NEM
-import qualified Data.OrdPSQ as PSQ
-import qualified Data.Sequence as Seq
-import qualified Data.Sequence.NonEmpty as NESeq
+import Data.Ord (comparing)
+import Data.Set (Set)
 import qualified Data.Set as S
-import qualified Data.Set.NonEmpty as NES
-import qualified Data.Text as T
-import qualified Data.Vector as V
-import qualified Linear as L
-import qualified Text.Megaparsec as P
-import qualified Text.Megaparsec.Char as P
-import qualified Text.Megaparsec.Char.Lexer as PP
+import Data.Tree (Forest, Tree (..))
+import qualified Data.Tree as Tree
+import Linear (V2 (..), V3 (..))
+import Safe.Foldable (maximumByMay)
 
-day23a :: _ :~> _
+connMap :: Ord a => [V2 a] -> Map a (Set a)
+connMap xs =
+  M.unionsWith
+    (<>)
+    [ M.fromList [(a, S.singleton b), (b, S.empty)]
+    | [a, b] <- sort . toList <$> xs
+    ]
+
+day23a :: [V2 String] :~> Int
 day23a =
   MkSol
-    { sParse = parseMaybe' $ sepByLines $ sequenceSepBy (V2 pAlphaNumWord  pAlphaNumWord) "-"
-        -- noFail $
-        --   lines
+    { sParse = parseMaybe' $ sepByLines $ sequenceSepBy (V2 pAlphaNumWord pAlphaNumWord) "-"
     , sShow = show
     , sSolve =
         noFail \xs ->
-          let conns = M.unionsWith (<>)
-                [ M.fromList [(a, S.singleton b), (b, S.singleton a)]
-                  | V2 a b <- xs
-                ]
-              triples = flip M.mapWithKey conns \a bs ->
-                              flip M.fromSet bs \b ->
-                                S.filter ((a `S.member`) . (conns M.!)) $ conns M.! b
-              tripleSet = S.fromList
-                [ S.fromList [ a, b, c ]
-                | (a, bs) <- M.toList triples
-                , (b, cs) <- M.toList bs
-                , c <- toList cs
-                ]
-           in countTrue (any ("t" `isPrefixOf`)) tripleSet
+          let conns = connMap xs
+           in countTrue (any ("t" `isPrefixOf`)) do
+                (a, bs) <- M.toList conns
+                b <- toList bs
+                c <- toList $ conns M.! b
+                guard $ c `S.member` bs
+                pure (V3 a b c)
     }
 
--- data StringChain = 
-
-day23b :: _ :~> _
+day23b :: [V2 String] :~> [String]
 day23b =
   MkSol
     { sParse = sParse day23a
-    -- , sShow = unlines . map Tree.drawTree
-    , sShow = intercalate "," . toList
-    , sSolve =
-        noFail \xs ->
-          -- let gr :: G.Gr () ()
-          --     gr = G.mkUGraph (foldMap (map encodeTwo . toList) xs) $
-          --           [ (encodeTwo a, encodeTwo b)
-          --             | V2 a b <- xs
-          --           ]
-          --     allNodes = foldMap (map encodeTwo . toList) xs
-          --  in fmap decodeTwo <$> G.dff [encodeTwo "ka"] gr
-          --  -- in map (map decodeTwo) $ G.scc gr
-          let conns = M.unionsWith (<>)
-                [ M.fromList [(a, S.singleton b), (b, S.singleton a)]
-                  | V2 a b <- xs
+    , sShow = intercalate ","
+    , sSolve = \xs -> do
+        let conns = connMap xs
+            clubsTree :: Forest String
+            clubsTree = flip Tree.unfoldForest (M.toList conns) \(a, cands) ->
+              ( a
+              , [ (b, cands `S.intersection` (conns M.! b))
+                | b <- toList cands
                 ]
-              clubsTree = M.keys conns <&> \k ->
-                  flip Tree.unfoldTree (k, S.empty) \(out, seen) ->
-                      (out, [ (b, S.insert out seen)
-                            | b <- toList $ conns M.! out
-                            , all (< b) seen
-                            , out < b
-                            , all (b `S.member`) (map (conns M.!) $ toList seen)
-                            ]
-                      )
-              clubs :: Set (Set String)
-              clubs = fold $ fmap (go S.empty) clubsTree
-                where
-                  go :: Set String -> Tree.Tree String -> Set (Set String)
-                  go seen (Tree.Node here there)
-                    | null there = S.singleton (S.insert here seen)
-                    | otherwise = fold $ go (S.insert here seen) <$> there
-          in  toList $ maximumBy (comparing S.size) clubs
-          -- in  clubsTree
+              )
+            clubs :: [[String]]
+            clubs = go clubsTree
+              where
+                go :: [Tree String] -> [[String]]
+                go ts
+                  | null ts = pure []
+                  | otherwise = do
+                      Node here there <- ts
+                      (here :) <$> go there
+        maximumByMay (comparing length) clubs
     }
-  where
-    encodeTwo [a,b] = fromJust do 
-      (_, a') <- charFinite a
-      (_, b') <- charFinite b
-      pure $ fromIntegral a' * 26 + fromIntegral b'
-    decodeTwo ((`divMod` 26) -> (a,b)) = [review _CharFinite (False, fromIntegral a), review _CharFinite (False, fromIntegral b)]
--- charFinite :: Char -> Maybe (Bool, Finite 26)
