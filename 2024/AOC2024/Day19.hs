6a6730046932e397feaf366d497b1c44dd4ce204
justin <justin@jle.im>
2024-12-29 15:10:44 -0800

    write as a hylo but it's slower

--- 2024/AOC2024/Day19.hs
+++ 2024/AOC2024/Day19.hs
@@ -6,23 +6,35 @@
 -- Portability : non-portable
 --
 -- Day 19.  See "AOC.Solver" for the types used in this module!
-module AOC2024.Day19 (
-  day19a,
-  day19b,
-)
+module AOC2024.Day19
 where
+
+-- (
+-- day19a,
+-- day19b,
+-- )
 
 import AOC.Common.Parser (pAlphaNumWord, parseMaybe')
 import AOC.Solver (type (:~>) (..))
 import Control.DeepSeq (NFData)
+import Control.Monad
 import Control.Monad (guard)
+import Control.Monad.Free
+import Data.Bifunctor
 import Data.Finite (Finite)
+import Data.Foldable
 import Data.Foldable (fold)
-import Data.Functor.Foldable (Corecursive (ana), Recursive (cata))
+import Data.Functor.Foldable hiding (fold)
 import Data.Functor.Foldable.TH (MakeBaseFunctor (makeBaseFunctor))
+import Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NE
+import Data.Map (Map)
 import qualified Data.Map as M
+import Data.Map.NonEmpty (NEMap)
+import qualified Data.Map.NonEmpty as NEM
 import Data.Maybe (mapMaybe)
-import Data.Semigroup (Sum (getSum))
+import Data.Semigroup
+import Data.These
 import qualified Data.Vector.Sized as SV
 import GHC.Generics (Generic)
 import GHC.TypeNats (KnownNat)
@@ -49,23 +61,91 @@ bindTrie t f = flip cata t \case
     Just x -> case f x of
       CT here' there' -> CT here' (SV.zipWith (<>) ctThereF there')
 
+fromMap :: KnownNat n => Map [Finite n] a -> NTrie n a
+fromMap = ana \mp ->
+  let (here, there) = flip M.foldMapWithKey mp \case
+        [] -> \x -> (Just (First x), mempty)
+        k : ks -> \x -> (mempty, SV.generate \i -> [(ks, x)] <$ guard (i == k))
+   in CTF (getFirst <$> here) (fmap M.fromList <$> there)
+
+fromMapForever ::
+  forall n a. (KnownNat n, Semigroup a) => NEMap (NonEmpty (Finite n)) a -> NTrie n a
+fromMapForever mp0 = ana (fromMapCoalg mp0) (That mp0)
+
+fromMapCoalg ::
+  forall n a.
+  (KnownNat n, Semigroup a) =>
+  NEMap (NonEmpty (Finite n)) a ->
+  These a (NEMap (NonEmpty (Finite n)) a) ->
+  NTrieF n a (These a (NEMap (NonEmpty (Finite n)) a))
+fromMapCoalg mp0 = \case
+  This x -> CTF (Just x) $ fmap separateMap <$> initialSplit x
+  That ks -> CTF Nothing $ fmap separateMap <$> splitTrie ks
+  These x ks ->
+    CTF (Just x) $
+      fmap separateMap <$> SV.zipWith combineMaybe (initialSplit x) (splitTrie ks)
+  where
+    initialSplit :: a -> SV.Vector n (Maybe (NEMap [Finite n] a))
+    initialSplit x = fmap (x <$) <$> splitTrie mp0
+    splitTrie :: NEMap (NonEmpty (Finite n)) a -> SV.Vector n (Maybe (NEMap [Finite n] a))
+    splitTrie mp = SV.generate \i ->
+      NEM.nonEmptyMap $
+        M.fromListWith
+          (<>)
+          [ (ks, x)
+          | (k :| ks, x) <- toList $ NEM.toList mp
+          , k == i
+          ]
+    combineMaybe Nothing Nothing = Nothing
+    combineMaybe (Just x) Nothing = Just x
+    combineMaybe Nothing (Just y) = Just y
+    combineMaybe (Just x) (Just y) = Just $ NEM.unionWith (<>) x y
+
+separateMap :: NEMap [Finite n] a -> These a (NEMap (NonEmpty (Finite n)) a)
+separateMap = first getFirst . NEM.foldMapWithKey go
+  where
+    go = \case
+      [] -> This . First
+      k : ks -> That . NEM.singleton (k :| ks)
+
+trieFromList :: KnownNat n => [([Finite n], a)] -> NTrie n a
+trieFromList = ana \mp ->
+  let (here, there) = flip foldMap mp \case
+        ([], x) -> (Just (First x), mempty)
+        (k : ks, x) -> (mempty, SV.generate \i -> [(ks, x)] <$ guard (i == k))
+   in CTF (getFirst <$> here) there
+
+-- trieFromListForever :: KnownNat n => [([Finite n], a)] -> NTrie n a
+-- trieFromListForever mp0 = flip ana mp0 \mp ->
+--   let (here, there) = flip foldMap mp \case
+--         ([], x) -> (Just (First x), SV.replicate (Just (first _ <$> mp0)))
+--         (k : ks, x) -> (mempty, SV.generate \i -> [(ks, x)] <$ guard (i == k))
+--    in CTF (getFirst <$> here) there
+
 singletonTrie :: KnownNat n => [Finite n] -> a -> NTrie n a
 singletonTrie str x = flip ana str \case
   [] -> CTF (Just x) (SV.replicate Nothing)
   c : cs -> CTF Nothing (SV.generate \i -> cs <$ guard (i == c))
 
 lookupTrie :: [Finite n] -> NTrie n a -> Maybe a
-lookupTrie str t = cata go t str
-  where
-    go CTF{..} = \case
-      [] -> ctHereF
-      c : cs -> ($ cs) =<< ctThereF `SV.index` c
+lookupTrie str t = cata lookupAlg t str
+
+lookupAlg :: NTrieF n a ([Finite n] -> Maybe a) -> [Finite n] -> Maybe a
+lookupAlg CTF{..} = \case
+  [] -> ctHereF
+  c : cs -> ($ cs) =<< ctThereF `SV.index` c
+
+foreverTrie' :: (KnownNat n, Semigroup w) => NonEmpty (NonEmpty (Finite n)) -> w -> NTrie n w
+foreverTrie' strs x = fromMapForever $ NEM.fromList ((,x) <$> strs)
 
 foreverTrie :: (KnownNat n, Semigroup w) => [[Finite n]] -> w -> NTrie n w
 foreverTrie strs x = infiniTrie
   where
-    tr = foldMap (`singletonTrie` x) strs
+    tr = trieFromList $ (,x) <$> strs
     infiniTrie = singletonTrie [] x <> (tr `bindTrie` const infiniTrie)
+
+buildable :: (KnownNat n, Semigroup a) => NEMap (NonEmpty (Finite n)) a -> [Finite n] -> Maybe a
+buildable mp0 = hylo lookupAlg (fromMapCoalg mp0) (That mp0)
 
 day19 :: Semigroup w => w -> ([w] -> Int) -> ([String], [String]) :~> Int
 day19 x agg =
@@ -79,9 +159,9 @@ day19 x agg =
           pure (ws, ls)
     , sShow = show
     , sSolve = \(ws, ls) -> do
-        ws' <- traverse toFinites ws
+        ws' <- NE.nonEmpty =<< traverse (NE.nonEmpty <=< toFinites) ws
         ls' <- traverse toFinites ls
-        pure $ agg $ mapMaybe (`lookupTrie` (foreverTrie @5) ws' x) ls'
+        pure $ agg $ mapMaybe (`lookupTrie` (foreverTrie' @5) ws' x) ls'
     }
   where
     toFinites = traverse $ flip M.lookup (M.fromList $ zip "wubrg" [0 ..])
---
+++ reflections/2024/day19.md
@@ -0,0 +1,1 @@
+This one can be solved in a fun way using an infinite n-trie.
