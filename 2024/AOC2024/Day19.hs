3f0b77c9cb1fdeef6717184af2379834732945db
justin <justin@jle.im>
2024-12-29 17:08:31 -0800

    more hylo oprtimize

--- 2024/AOC2024/Day19.hs
+++ 2024/AOC2024/Day19.hs
@@ -13,65 +13,67 @@ module AOC2024.Day19 (
 where
 
 import AOC.Common.Parser (pAlphaNumWord, parseMaybe')
-import AOC.Solver (type (:~>) (..))
+import AOC.Solver (noFail, type (:~>) (..))
 import Control.DeepSeq (NFData)
-import Data.Finite (Finite)
+import Data.Char
 import Data.Foldable (fold, toList)
 import Data.Functor.Foldable hiding (fold)
 import Data.Functor.Foldable.TH (MakeBaseFunctor (makeBaseFunctor))
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IM
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe (mapMaybe)
 import Data.Semigroup
 import Data.Set (Set)
 import qualified Data.Set as S
-import qualified Data.Vector.Sized as SV
 import GHC.Generics (Generic)
-import GHC.TypeNats (KnownNat)
 import qualified Text.Megaparsec as P
 import qualified Text.Megaparsec.Char as P
 
-data NTrie n a = CT {ctHere :: Maybe a, ctThere :: SV.Vector n (NTrie n a)}
+data CharTrie a = CT {ctHere :: Maybe a, ctThere :: IntMap (CharTrie a)}
   deriving stock (Show, Functor, Traversable, Foldable, Generic)
 
-deriving anyclass instance NFData a => NFData (NTrie n a)
+deriving anyclass instance NFData a => NFData (CharTrie a)
 
-makeBaseFunctor ''NTrie
+makeBaseFunctor ''CharTrie
 
 fromMapCoalg ::
-  forall n a.
-  (KnownNat n, Semigroup a) =>
-  Set [Finite n] ->
-  Map [Finite n] a ->
-  NTrieF n a (Map [Finite n] a)
+  forall a.
+  (Semigroup a) =>
+  Set [Int] ->
+  Map [Int] a ->
+  CharTrieF a (Map [Int] a)
 fromMapCoalg mp0 = \ks ->
   let x = M.lookup [] ks
       reAdd = case x of
         Nothing -> id
-        Just y -> SV.zipWith (M.unionWith (<>)) (M.fromSet (const y) <$> initialSplit)
+        Just y -> IM.unionWith (M.unionWith (<>)) (M.fromSet (const y) <$> initialSplit)
    in CTF x $ reAdd (splitTrie ks)
   where
-    initialSplit :: SV.Vector n (Set [Finite n])
-    initialSplit = SV.generate \i ->
-      S.fromDistinctAscList
-        [ ks
-        | k : ks <- toList mp0
-        , k == i
-        ]
-    splitTrie :: Map [Finite n] a -> SV.Vector n (Map [Finite n] a)
-    splitTrie mp = SV.generate \i ->
-      M.fromDistinctAscList
-        [ (ks, x)
-        | (k : ks, x) <- M.toList mp
-        , k == i
-        ]
+    initialSplit :: IntMap (Set [Int])
+    initialSplit =
+      S.fromDistinctDescList . ($ [])
+        <$> IM.fromAscListWith
+          (<>)
+          [ (k, (ks :))
+          | k : ks <- toList mp0
+          ]
+    splitTrie :: Map [Int] a -> IntMap (Map [Int] a)
+    splitTrie mp =
+      M.fromDistinctDescList . ($ [])
+        <$> IM.fromAscListWith
+          (.)
+          [ (k, ((ks, x) :))
+          | (k : ks, x) <- M.toList mp
+          ]
 
-lookupAlg :: NTrieF n a ([Finite n] -> Maybe a) -> [Finite n] -> Maybe a
+lookupAlg :: CharTrieF a ([Int] -> Maybe a) -> [Int] -> Maybe a
 lookupAlg CTF{..} = \case
   [] -> ctHereF
-  c : cs -> (ctThereF `SV.index` c) cs
+  c : cs -> ($ cs) =<< IM.lookup c ctThereF
 
-buildable :: (KnownNat n, Semigroup a) => a -> Set [Finite n] -> [Finite n] -> Maybe a
+buildable :: (Semigroup a) => a -> Set [Int] -> [Int] -> Maybe a
 buildable x0 mp0 = hylo lookupAlg (fromMapCoalg mp0) (M.fromSet (const x0) mp0)
 
 day19 :: Semigroup w => w -> ([w] -> Int) -> ([String], [String]) :~> Int
@@ -85,14 +87,10 @@ day19 x agg =
           ls <- pAlphaNumWord `P.sepBy` P.newline
           pure (ws, ls)
     , sShow = show
-    , sSolve = \(ws, ls) -> do
-        ws' <- traverse toFinites ws
-        ls' <- traverse toFinites ls
-        pure $ agg $ mapMaybe (buildable x (S.fromList ws')) ls'
+    , sSolve = noFail \(ws, ls) ->
+        agg $
+          mapMaybe (buildable x (S.fromList (map ord <$> ws)) . map ord) ls
     }
-  where
-    toFinites :: String -> Maybe [Finite 5]
-    toFinites = traverse $ flip M.lookup (M.fromList $ zip "wubrg" [0 ..])
 
 day19a :: ([String], [String]) :~> Int
 day19a = day19 () length
