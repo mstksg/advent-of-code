5a3439f3b27360e818ad824b2840c9b91266c751
justin <justin@jle.im>
2025-01-01 00:39:50 -0800

    day 22 reflections and cleanup

--- 2024/AOC2024/Day22.hs
+++ 2024/AOC2024/Day22.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC2024.Day22
 -- License     : BSD3
@@ -9,87 +6,53 @@
 -- Portability : non-portable
 --
 -- Day 22.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 module AOC2024.Day22 (
   day22a,
   day22b,
 )
 where
 
-import AOC.Prelude
-import Data.Bits
-import qualified Data.Graph.Inductive as G
+import AOC.Common (strictIterate, (!!!))
+import AOC.Common.Parser (pDecimal, parseMaybe', sepByLines)
+import AOC.Solver (noFail, type (:~>) (..))
+import Data.Bits (Bits (shift, xor, (.&.)))
+import Data.Foldable (Foldable (toList))
 import qualified Data.IntMap as IM
-import qualified Data.IntMap.NonEmpty as NEIM
-import qualified Data.IntSet as IS
-import qualified Data.IntSet.NonEmpty as NEIS
-import qualified Data.List.NonEmpty as NE
-import qualified Data.List.PointedList as PL
-import qualified Data.List.PointedList.Circular as PLC
-import qualified Data.Map as M
-import qualified Data.Map.NonEmpty as NEM
-import qualified Data.OrdPSQ as PSQ
-import qualified Data.Sequence as Seq
-import qualified Data.Sequence.NonEmpty as NESeq
-import qualified Data.Set as S
-import qualified Data.Set.NonEmpty as NES
-import qualified Data.Text as T
-import qualified Data.Vector as V
-import qualified Linear as L
-import qualified Text.Megaparsec as P
-import qualified Text.Megaparsec.Char as P
-import qualified Text.Megaparsec.Char.Lexer as PP
-
-day22a :: _ :~> _
-day22a =
-  MkSol
-    { sParse =
-        parseMaybe' $
-          sepByLines pDecimal
-    , -- noFail $
-      --   lines
-      sShow = show
-    , sSolve =
-        noFail $
-          sum . map ((!! 2000) . iterate step)
-    }
+import Safe.Foldable (maximumMay)
 
 step :: Int -> Int
-step n = n'''
+step = prune . phase3 . prune . phase2 . prune . phase1
   where
-    n' = prune $ (n `shift` 6) `xor` n
-    n'' = prune $ (n' `shift` (-5)) `xor` n'
-    n''' = prune $ (n'' `shift` 11) `xor` n''
+    phase1 n = (n `shift` 6) `xor` n
+    phase2 n = (n `shift` (-5)) `xor` n
+    phase3 n = (n `shift` 11) `xor` n
     prune = (.&. 16777215)
 
-day22b :: _ :~> _
+day22a :: [Int] :~> Int
+day22a =
+  MkSol
+    { sParse = parseMaybe' $ sepByLines pDecimal
+    , sShow = show
+    , sSolve = noFail $ sum . map ((!!! 2000) . strictIterate step)
+    }
+
+day22b :: [Int] :~> Int
 day22b =
   MkSol
     { sParse = sParse day22a
     , sShow = show
-    , sSolve =
-        noFail $ \xs ->
-          let serieses =
-                xs <&> \x ->
-                  let ps = take 2000 $ map (`mod` 10) $ iterate step x
-                      dPs = zipWith (\p0 p1 -> (p1, p1 - p0)) ps (drop 1 ps)
-                      windows = slidingWindows 4 dPs <&> \w -> (encodeSeq $ snd <$> w, fst $ last (toList w))
-                      seqMap = IM.fromListWith (const id) windows
-                   in seqMap
-              bests = toList $ IM.unionsWith (+) serieses
-           in maximum bests
-          -- bests = M.unionsWith (<>) $ map (fmap (:[])) serieses
-          -- in maximumBy (comparing (sum . snd)) (M.toList bests)
+    , sSolve = maximumMay . toList . IM.unionsWith (+) . map genSeries
     }
   where
-    encodeSeq = sum . zipWith (\i x -> x * 19^(i :: Int)) [0..] . map (+ 9) . toList
+    encodeSeq = sum . zipWith (\i x -> x * 19 ^ (i :: Int)) [0 ..] . map (+ 9)
+    genSeries = IM.fromListWith (const id) . chompChomp . take 2000 . map (`mod` 10) . strictIterate step
+      where
+        chompChomp :: [Int] -> [(Int, Int)]
+        chompChomp (a : b : c : d : e : fs) =
+          (encodeSeq [da, db, dc, dd], e) : chompChomp (b : c : d : e : fs)
+          where
+            da = b - a
+            db = c - b
+            dc = d - c
+            dd = e - d
+        chompChomp _ = []
---
+++ reflections/2024/day22.md
@@ -0,0 +1,48 @@
+First let's set up the RNG step:
+
+```haskell
+step :: Int -> Int
+step = prune . phase3 . prune . phase2 . prune . phase1
+  where
+    phase1 n = (n `shift` 6) `xor` n
+    phase2 n = (n `shift` (-5)) `xor` n
+    phase3 n = (n `shift` 11) `xor` n
+    prune = (.&. 16777215)
+```
+
+Part 1 is just running and summing:
+
+
+```haskell
+part1 :: [Int] -> Int
+part1 = sum . map ((!! 2000) . iterate)
+```
+
+Part 2 is a little more interesting. We want to make a map of 4-sequences to
+the first price they would get. On a chain of iterations, we can iteratively
+chomp on runs of 4:
+
+```haskell
+chompChomp :: [Int] -> [([Int], Int)]
+chompChomp (a : b : c : d : e : fs) =
+    ([da, db, dc, dd], e) : chompChomp (b : c : d : e : fs)
+  where
+    da = b - a
+    db = c - b
+    dc = d - c
+    dd = e - d
+chompChomp _ = []
+
+priceForChain :: Int -> Map [Int] Int
+priceForChain = M.fromListWith (const id) . chompChomp . take 2000 . map (`mod` 10) . iterate step
+```
+
+Then we can sum all of the sequence prices and get the maximum:
+
+```haskell
+part2 :: [Int] -> Int
+part2 = maximum . M.elems . M.fromListWith (+) . map priceForChain
+```
+
+I'm not super happy with the fact that this takes 3 seconds (even after
+optimizing to using `IntMap` on a base-19 encoding of the sequence).
