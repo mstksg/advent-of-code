665e1831554e887f9b13f4c268f320dbd2942dd9
justin <justin@jle.im>
2025-11-30 21:56:51 -0800

    cleaned up sol

--- 2025/AOC2025/Day01.hs
+++ 2025/AOC2025/Day01.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC2025.Day01
 -- License     : BSD3
@@ -9,82 +6,51 @@
 -- Portability : non-portable
 --
 -- Day 1.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 module AOC2025.Day01 (
   day01a,
   day01b,
 )
 where
 
-import AOC.Prelude
-import qualified Data.Graph.Inductive as G
-import qualified Data.IntMap as IM
-import qualified Data.IntMap.NonEmpty as NEIM
-import qualified Data.IntSet as IS
-import qualified Data.IntSet.NonEmpty as NEIS
-import qualified Data.List.NonEmpty as NE
-import qualified Data.List.PointedList as PL
-import qualified Data.List.PointedList.Circular as PLC
-import qualified Data.Map as M
-import qualified Data.Map.NonEmpty as NEM
-import qualified Data.OrdPSQ as PSQ
-import qualified Data.Sequence as Seq
-import qualified Data.Sequence.NonEmpty as NESeq
-import qualified Data.Set as S
-import qualified Data.Set.NonEmpty as NES
-import qualified Data.Text as T
-import qualified Data.Vector as V
-import qualified Linear as L
-import qualified Text.Megaparsec as P
-import qualified Text.Megaparsec.Char as P
-import qualified Text.Megaparsec.Char.Lexer as PP
+import AOC.Common (countTrue)
+import AOC.Solver (noFail, type (:~>) (..))
+import Data.Finite (modulo)
+import Data.List (scanl')
+import Data.Maybe (mapMaybe)
+import Data.Traversable (mapAccumL)
+import Text.Read (readMaybe)
 
-day01a :: _ :~> _
+day01a :: [Integer] :~> Int
 day01a =
   MkSol
-    { sParse =
-        noFail $
-          map (\case 'R':n -> read n; 'L':n -> negate $ read n) . lines
+    { sParse = traverse (readMaybe . mapMaybe align) . lines
     , sShow = show
     , sSolve =
         noFail $
-          length . filter (== 0) . map (`mod` 100) . tail . scanl' (+) 50
+          countTrue (== 0) . scanl' (+) 50 . map (modulo @100)
     }
+  where
+    align = \case
+      'R' -> Nothing
+      'L' -> Just '-'
+      d -> Just d
 
-day01b :: _ :~> _
+day01b :: [Integer] :~> Integer
 day01b =
   MkSol
     { sParse = sParse day01a
     , sShow = show
     , sSolve =
-        noFail $ 
-          -- map (`mod` 100) . concat . snd . mapAccumL go (50 :: Int)
-          length . filter (== 0) . map (`mod` 100) . concat . snd . mapAccumL go (50 :: Int)
+        noFail $ sum . snd . mapAccumL go 50
     }
   where
-    go curr bump
-      | bump > 0 = swap ([curr + 1 .. curr + bump], curr+bump)
-      | otherwise = swap (reverse [curr + bump .. curr - 1], curr+bump)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    go curr bump = (m, hits)
+      where
+        (d, m) = (curr + bump) `divMod` 100
+        hits
+          | bump > 0 = d
+          | otherwise = case (curr, m) of
+              (0, 0) -> abs d + 1
+              (0, _) -> abs d - 1
+              (_, 0) -> abs d + 1
+              (_, _) -> abs d
