24a71b1d0a10b89250376263b73989a3b97b1f5e
justin <justin@jle.im>
2025-12-01 22:31:46 -0800

    day 2 reflection

--- 2025/AOC2025/Day02.hs
+++ 2025/AOC2025/Day02.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC2025.Day02
 -- License     : BSD3
@@ -9,76 +6,42 @@
 -- Portability : non-portable
 --
 -- Day 2.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 module AOC2025.Day02 (
   day02a,
   day02b,
-  isDupper
 )
 where
 
-import AOC.Prelude
-import qualified Data.Graph.Inductive as G
-import qualified Data.IntMap as IM
-import qualified Data.IntMap.NonEmpty as NEIM
+import AOC.Common (listTup)
+import AOC.Solver (noFail, type (:~>) (..))
+import Control.Monad ((<=<))
+import Data.IntSet (IntSet)
 import qualified Data.IntSet as IS
-import qualified Data.IntSet.NonEmpty as NEIS
-import qualified Data.List.NonEmpty as NE
-import qualified Data.List.PointedList as PL
-import qualified Data.List.PointedList.Circular as PLC
-import qualified Data.Map as M
-import qualified Data.Map.NonEmpty as NEM
-import qualified Data.OrdPSQ as PSQ
-import qualified Data.Sequence as Seq
-import qualified Data.Sequence.NonEmpty as NESeq
-import qualified Data.Set as S
-import qualified Data.Set.NonEmpty as NES
-import qualified Data.Text as T
-import qualified Data.Vector as V
-import qualified Linear as L
-import qualified Text.Megaparsec as P
-import qualified Text.Megaparsec.Char as P
-import qualified Text.Megaparsec.Char.Lexer as PP
+import Data.List.Split (splitOn)
+import Text.Read (readMaybe)
 
-day02a :: [(Int, Int)] :~> _
-day02a =
+-- | repDigits 3 567 = 567567567
+repDigits :: Int -> Int -> Int
+repDigits n = read . concat . replicate n . show
+
+-- | Up to 1e11
+repN :: [Int] -> IntSet
+repN ns = flip foldMap ns $ \n ->
+  IS.fromAscList . takeWhile (< 1e11) . map (repDigits n) $ [1 ..]
+
+day02 :: [Int] -> [(Int, Int)] :~> Int
+day02 ns =
   MkSol
     { sParse =
-          traverse (listTup . map read . splitOn "-") . splitOn ","
+        traverse (listTup <=< traverse readMaybe . splitOn "-") . splitOn ","
     , sShow = show
     , sSolve =
         noFail $
-          sum . filter (isDup . show) . concatMap (\(x, y) -> [x .. y])
-    }
-  where
-    isDup xs = a == b && even (length xs)
-      where
-        (a, b) = splitAt (length xs `div` 2) xs
-
-day02b :: _ :~> _
-day02b =
-  MkSol
-    { sParse = sParse day02a
-    , sShow = show
-    , sSolve =
-        noFail $
-          sum . filter (isDupper . show) . concatMap (\(x, y) -> [x .. y])
+          IS.foldl' (+) 0 . foldMap (IS.intersection (repN ns) . IS.fromRange)
     }
 
-isDupper xs = not $ null $ do
-  (a,b) <- zip (inits xs) (tails xs)
-  guard $ not (null a)
-  guard $ not (null b)
-  guard $ length b `mod` length a == 0
-  let match = zip (cycle a) b
-  guard $ all (uncurry (==)) match
+day02a :: [(Int, Int)] :~> Int
+day02a = day02 [2]
+
+day02b :: [(Int, Int)] :~> Int
+day02b = day02 [2 .. 11]
---
+++ reflections/2025/day02.md
@@ -0,0 +1,27 @@
+You can do this nicely using the `IntSet` type in the *containers* library,
+with `IS.fromRange :: (Int, Int) -> IntSet`. Then you can just turn the ranges
+`IntSet`s and intersect them with the IntSet of all invalid IDs.
+
+```haskell
+-- | repDigits 3 567 = 567567567
+repDigits :: Int -> Int -> Int
+repDigits n = read . concat . replicate n . show
+
+-- | All duplicated IDs up to 1e11
+rep2 :: IntSet
+rep2 = IS.fromAscList . takeWhile (< 1e11) . map (repDigits 2) $ [1 ..]
+
+part1 :: [(Int, Int)] -> Int
+part1 = IS.foldl' (+) 0 . foldMap (IS.intersection rep2 . IS.fromRange)
+```
+
+And you can union together `rep2`, `rep3`, etc. too:
+
+```haskell
+repN :: IntSet
+repN = flip foldMap [1..11] $ \n ->
+  IS.fromAscList . takeWhile (< 1e11) . map (repDigits n) $ [1 ..]
+
+part2 :: [(Int, Int)] -> Int
+part2 = IS.foldl' (+) 0 . foldMap (IS.intersection repN . IS.fromRange)
+```
