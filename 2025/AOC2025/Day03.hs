fb925459a2488eaefdad9d319fba207bdcdebe53
justin <justin@jle.im>
2025-12-02 21:55:48 -0800

    initial

--- 2025/AOC2025/Day03.hs
+++ 2025/AOC2025/Day03.hs
@@ -20,12 +20,14 @@
 --     types @_ :~> _@ with the actual types of inputs and outputs of the
 --     solution.  You can delete the type signatures completely and GHC
 --     will recommend what should go in place of the underscores.
-module AOC2025.Day03 (
--- day03a,
--- day03b
-
-)
-where
+module AOC2025.Day03 where 
+  -- (
+  -- day03a,
+  -- day03b,
+  -- go2,
+  -- go12
+-- )
+-- where
 
 import AOC.Prelude
 import qualified Data.Graph.Inductive as G
@@ -55,12 +57,18 @@ day03a =
   MkSol
     { sParse =
         noFail $
-          lines
+          map (map digitToInt) . lines
     , sShow = show
     , sSolve =
         noFail $
-          id
+          sum . map go2
     }
+  where
+go2 xs = maximum
+  [ x * 10 + y
+  | x: ys <- tails xs
+  , y:_ <- tails ys
+  ]
 
 day03b :: _ :~> _
 day03b =
@@ -68,6 +76,75 @@ day03b =
     { sParse = sParse day03a
     , sShow = show
     , sSolve =
-        noFail $
-          id
+            fmap sum . traverse (readMaybe @Int . map intToDigit <=< listToMaybe . evalStateT runItAll)
+          -- sum . map go12
     }
+  where
+
+    go xs = find (inPat xs) (candidates xs)
+      where
+        inPat _ [] = True
+        inPat [] (_:_) = False
+        inPat (x:xs') (c:cs)
+          | x == c = inPat xs' cs
+          | otherwise = inPat xs' (c:cs)
+
+pickAtN :: Int -> [Int] -> [[Int]]
+pickAtN n xs = [ xs'
+                | x:xs' <- tails xs
+               , x == n
+               ]
+
+descendPicks :: [Int] -> [(Int, [Int])]
+descendPicks xs = do 
+  n <- reverse [1..9]
+  picked <- pickAtN n xs
+  pure (n, picked)
+
+runItAll :: StateT [Int] [] [Int]
+runItAll = replicateM 12 (StateT descendPicks)
+
+candidates :: [Int] -> [[Int]]
+candidates = selections . reverse . sort
+-- -- pick max candidates -- and position
+-- -- pickMax :: Seq a -> Maybe (Int, a)
+
+-- maxIx :: [a] -> Maybe
+-- go12' xs = _
+--   where
+--     firstFound n = 
+
+-- go12 = undefined
+-- 9869755494885236452767647548826258555668838497687557968652485746757835445145546687436356799481798587
+
+selections xs =
+      [ [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12]
+      | x1: xs1 <- tails xs
+      , x2: xs2 <- tails xs1
+      , x3: xs3 <- tails xs2
+      , x4: xs4 <- tails xs3
+      , x5: xs5 <- tails xs4
+      , x6: xs6 <- tails xs5
+      , x7: xs7 <- tails xs6
+      , x8: xs8 <- tails xs7
+      , x9: xs9 <- tails xs8
+      , x10: xs10 <- tails xs9
+      , x11: xs11 <- tails xs10
+      , x12: xs12 <- tails xs11
+      ]
+
+go12 xs = traceShowId . read . map intToDigit $ maximum
+      [ [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12]
+      | x1: xs1 <- tails xs
+      , x2: xs2 <- tails xs1
+      , x3: xs3 <- tails xs2
+      , x4: xs4 <- tails xs3
+      , x5: xs5 <- tails xs4
+      , x6: xs6 <- tails xs5
+      , x7: xs7 <- tails xs6
+      , x8: xs8 <- tails xs7
+      , x9: xs9 <- tails xs8
+      , x10: xs10 <- tails xs9
+      , x11: xs11 <- tails xs10
+      , x12: xs12 <- tails xs11
+      ]
