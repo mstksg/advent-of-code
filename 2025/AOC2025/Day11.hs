6dcb0d42b1a8578a6d8a4dd3fe8df8404e57011b
justin <justin@jle.im>
2025-12-11 00:18:45 -0800

    double memo

--- 2025/AOC2025/Day11.hs
+++ 2025/AOC2025/Day11.hs
@@ -48,25 +48,25 @@ import qualified Linear as L
 import qualified Text.Megaparsec as P
 import qualified Text.Megaparsec.Char as P
 import qualified Text.Megaparsec.Char.Lexer as PP
+import Data.Bitraversable
 
 day11a :: [(String, [String])] :~> _
 day11a =
   MkSol
     { sParse =
-          traverse (fmap (first init) . uncons . words) . lines
+          traverse (bitraverse initMay pure <=< uncons . words) . lines
     , sShow = show
     , sSolve =
-        noFail \xs -> 
-          let conns :: Map String (Set String)
-              conns = M.fromList $ map (second S.fromList) xs
-              toOut :: Map String (Set [String])
-              toOut = flip M.mapWithKey conns \x nx ->
-                flip foldMap (S.toList nx) \y ->
-                    if y == "out"
-                       then S.singleton [x, "out"]
-                       else maybe S.empty (S.map (x:)) $ M.lookup y toOut
-           in S.size $ pathsTo conns S.empty "you" "out"
+        noFail \xs ->
+          pathsTo' (M.fromList xs) "you" "out"
     }
+
+flipMap' :: Map String (Set String) -> Map String (Set String)
+flipMap' mp = M.fromListWith (<>)
+  [ (x, S.singleton k)
+    | (k, xs) <- M.toList mp
+  , x <- S.toList xs
+  ]
 
 day11b :: _ :~> _
 day11b =
@@ -75,37 +75,83 @@ day11b =
     , sShow = show
     , sSolve =
         noFail \xs ->
-          let conns :: Map String (Set String)
-              conns = M.fromList $ map (second S.fromList) xs
-              svrToDac :: Set [String]
-              svrToDac = pathsTo conns (S.fromList ["out", "fft"]) "svr" "dac"
-              dacToFft :: Set [String]
-              dacToFft = pathsTo conns (S.fromList ["svr", "out"]) "dac" "fft"
-              fftToOut :: Set [String]
-              fftToOut = pathsTo conns (S.fromList ["svr", "dac"]) "fft" "out"
-
-              svrToFft :: Set [String]
-              svrToFft = pathsTo conns (S.fromList ["out", "dac"]) "svr" "fft"
-              fftToDac :: Set [String]
-              fftToDac = pathsTo conns (S.fromList ["svr", "out"]) "fft" "dac"
-              dacToOut :: Set [String]
-              dacToOut = pathsTo conns (S.fromList ["svr", "fft"]) "dac" "out"
-          in product $ map S.size [svrToFft, fftToDac, dacToOut]
-          -- in (svrToDac, dacToFft, fftToOut)
-              -- toOut :: Map String (Set [String])
-              -- toOut = flip M.mapWithKey conns \x nx ->
-              --   flip foldMap (S.toList nx) \y ->
-              --       if y == "out"
-              --          then S.singleton [x, "out"]
-              --          else maybe S.empty (S.map (x:)) $ M.lookup y toOut
-           -- in S.size $ S.filter (\q -> (== 2) . S.size $ S.intersection (S.fromList ["dac","fft"]) $ S.fromList $ traceShowId q) (toOut M.! "svr")
+          let toVisit = S.fromList ["dac","fft"]
+           in pathsTo' (M.fromList xs `addVisited` toVisit) ("svr", toVisit) ("out", S.empty)
     }
 
-pathsTo :: Map String (Set String) -> Set String -> String -> String -> Set [String]
+pathsTo :: Map String (Set String) -> Set String -> String -> String -> [[String]]
 pathsTo conns takeout start end = res M.! start
   where
     conns' = (`S.difference` takeout) <$> (conns `M.withoutKeys` takeout)
     res = flip M.mapWithKey conns \x nx -> flip foldMap nx \y ->
         if y == end
-           then S.singleton [x, end]
-           else maybe S.empty (S.map (x:)) $ M.lookup y res
+           then [[x, end]]
+           else maybe [] (map (x:)) $ M.lookup y res
+
+pathsToAll :: Map String (Set String) -> Map String (Map String Int)
+pathsToAll conns = flip M.fromSet (fold conns) \end ->
+  let res = flip M.mapWithKey conns \start nexts -> 
+        sum 
+          [ if y == end then 1 else M.findWithDefault 0 y res
+            | y <- S.toList nexts
+          ]
+    in res
+
+pathsTo' :: (Ord a, Foldable t) => Map a (t a) -> a -> a -> Int
+pathsTo' conns start end = res M.! start
+  where
+    res = flip M.mapWithKey conns \x nx -> getSum $ flip foldMap nx \y ->
+        if y == end
+           then 1
+           else Sum $ M.findWithDefault 0 y res
+
+addVisited :: Ord a => Map a [a] -> Set a -> Map (a, Set a) [(a, Set a)]
+addVisited conns required = M.fromList
+    [ ((x, subset), map (, S.delete x subset) xs)
+      | (x, xs) <- M.toList conns
+    , subset <- S.fromDistinctAscList <$> filterM (\_ -> [False, True]) (S.toAscList required)
+    ]
+
+chomperoni :: Map String (Set String) -> Set String -> String -> String -> Int
+chomperoni conns visitMe start end = go visitMe start
+  where
+    go :: Set String -> String -> Int
+    go toSee curr
+      | curr == end = if S.null toSee then 1 else 0
+      | otherwise = sum do
+          next <- S.toList $ M.findWithDefault S.empty curr conns
+          pure $ go (S.delete curr toSee) next
+
+data SearchState = S { leftOver :: !(Set String), current :: !String }
+  deriving stock Show
+
+searchBfs :: Map String (Set String) -> Set String -> String -> String -> Int
+searchBfs conns visitMe start end = go (Seq.singleton (S visitMe start)) 0
+  where
+    go Seq.Empty tot = tot
+    go (S toSee curr :<| xs) !tot
+      | curr == end = go xs $ if S.null toSee then tot + 1 else tot
+      | otherwise =
+          let nexts = S (S.delete curr toSee) <$> foldMap toList (M.lookup curr conns)
+           in go (xs <> Seq.fromList nexts) tot
+
+-- data SearchState2 = S { leftOver2 :: !(Set String), pathsToHere :: !Int }
+--   deriving stock Show
+
+-- searchBfs' :: Map String (Set String) -> Set String -> String -> String -> Int
+-- searchBfs' conns visitMe start end = go (M.singleton start (S2 visitMe pathsToHere)) 0
+--   where
+--     go queue tot = case M.minViewWithKey $ traceShowId queue of
+--       Nothing -> tot
+--       Just ((curr, toSee), xs)
+--         | curr == end -> go xs $ if S.null toSee then tot + 1 else tot
+--         | otherwise ->
+--           let nexts = M.fromSet (\_ -> S.delete curr toSee) $ M.findWithDefault S.empty curr conns
+--            in go (M.unionWith (\x y -> if S.size x < S.size y then x else y) nexts xs) tot
+            
+    -- go Seq.Empty tot = tot
+    -- go (S toSee curr :<| xs) !tot
+    --   | curr == end = go xs $ traceShowId $ if S.null toSee then tot + 1 else tot
+    --   | otherwise =
+    --       let nexts = traceShowId $ S (S.delete curr toSee) <$> foldMap toList (M.lookup curr conns)
+    --        in go (xs <> Seq.fromList nexts) tot
