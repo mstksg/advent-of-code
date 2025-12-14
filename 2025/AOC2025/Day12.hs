ecce9d51e22ebd5a6d28676904b23bb3c543d0a3
justin <justin@jle.im>
2025-12-14 14:39:42 -0800

    clean up day 12

--- 2025/AOC2025/Day12.hs
+++ 2025/AOC2025/Day12.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC2025.Day12
 -- License     : BSD3
@@ -9,127 +6,35 @@
 -- Portability : non-portable
 --
 -- Day 12.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 module AOC2025.Day12 (
-day12a,
-day12b
+  day12a,
 )
 where
 
-import AOC.Prelude
-import qualified Data.Graph.Inductive as G
-import qualified Data.IntMap as IM
-import qualified Data.IntMap.NonEmpty as NEIM
-import qualified Data.IntSet as IS
-import qualified Data.IntSet.NonEmpty as NEIS
-import qualified Data.List.NonEmpty as NE
-import qualified Data.List.PointedList as PL
-import qualified Data.List.PointedList.Circular as PLC
-import qualified Data.Map as M
-import qualified Data.Map.NonEmpty as NEM
-import qualified Data.OrdPSQ as PSQ
-import qualified Data.Sequence as Seq
-import qualified Data.Sequence.NonEmpty as NESeq
+import AOC.Common (countTrue, listV2)
+import AOC.Common.Point (Point, parseAsciiSet)
+import AOC.Solver (noFail, (:~>) (..))
+import Control.Monad ((<=<))
+import Data.Bitraversable (bitraverse)
+import Data.List (uncons)
+import Data.List.Split (splitOn)
+import Data.Set (Set)
 import qualified Data.Set as S
-import qualified Data.Set.NonEmpty as NES
-import qualified Data.Text as T
-import qualified Data.Vector as V
-import qualified Linear as L
-import qualified Text.Megaparsec as P
-import qualified Text.Megaparsec.Char as P
-import qualified Text.Megaparsec.Char.Lexer as PP
+import Safe (initMay, lastMay)
+import Text.Read (readMaybe)
 
-pVec :: String -> Point
-pVec xs = case read <$> splitOn "x" (init xs) of
-                  [x,y] -> V2 x y
-              
+pVec :: String -> Maybe Point
+pVec = listV2 <=< traverse readMaybe . splitOn "x" <=< initMay
 
-day12a :: _ :~> _
+day12a :: ([Set Point], [(Point, [Int])]) :~> Int
 day12a =
   MkSol
-    { sParse =
-        noFail $ \xs ->
-          let chunkies = splitOn [""] $ lines xs
-              blockies = map (parseAsciiSet (== '#') . unlines . tail) $ init chunkies
-              thingies = map (bimap pVec (map (read @Int)). fromJust . uncons . words) $ last chunkies
-           in (blockies, thingies)
+    { sParse = \xs -> do
+        let chunkies = splitOn [""] $ lines xs
+        blockies <- map (parseAsciiSet (== '#') . unlines . drop 1) <$> initMay chunkies
+        thingies <- traverse (bitraverse pVec (traverse readMaybe) <=< uncons . words) =<< lastMay chunkies
+        pure (blockies, thingies)
     , sShow = show
-    , sSolve =
-        noFail \(blocks, areas) -> countTrue (\(bounds, ns) -> (product bounds) >= (sum $ zipWith (*) (map S.size blocks) ns)) areas
-        -- noFail \(blocks, areas) -> map (\(bounds, ns) -> (product bounds, sum $ zipWith (*) (map S.size blocks) ns)) areas
-        -- noFail \(blocks, areas) -> countTrue (go blocks) areas
-    }
-  where
-    -- go :: [Set Point] -> (Point, [Int]) -> Bool
-    -- go blocks (bounds, repeats) = not . null $ execStateT (zipWithM go2 blocks repeats) S.empty
-    --   where
-    --     go2 block rep = replicateM rep $ StateT (map ((),) . placer bounds block)
-    go :: [Set Point] -> (Point, [Int]) -> Bool
-    go blocks (bounds, repeats) = isJust $ traceShowId $ bfs
-          (popper bounds)
-          state0
-          (M.null . sToPlace)
-      where
-        state0 = S (M.filter (> 0) $ M.fromList (zip blocks repeats)) S.empty
-
-placer :: Point -> Set Point -> Set Point -> [Set Point]
-placer bounds x placed = nubOrd $ do
-    rot <- S.toList . S.fromList $ toList allD8 <&> \d -> map ((+ 1) . orientPoint d . subtract 1) $ S.toList x
-    coord <- toList $ fillBoundingBox (V2 0 (bounds - 3))
-    let place = S.fromList $ (+ coord) <$> rot
-    guard . null $ placed `S.intersection` place
-    pure $ placed <> place
-
-data SearchState = S { sToPlace :: Map (Set Point) Int, sPlaced :: Set Point }
-  deriving stock (Show, Eq, Ord)
-
-popper :: Point -> SearchState -> Set SearchState
-popper bounds (S toPlace placed) = traceShowId $ S.fromList do
-    (x, _) <- M.toList toPlace
-    rot <- S.toList . S.fromList $ toList allD8 <&> \d -> map ((+ 1) . orientPoint d . subtract 1) $ S.toList x
-    coord <- toList $ fillBoundingBox (V2 0 (bounds - 3))
-    let place = S.fromList $ (+ coord) <$> rot
-    guard . null $ placed `S.intersection` place
-    let placed' = placed <> place
-        toPlace' = M.update (mfilter (> 0) . Just . subtract 1) x toPlace
-    pure $ S toPlace' placed'
-
-allPlacements :: Point -> Set Point -> Set (Set Point)
-allPlacements bounds x = S.fromList do
-    rot <- S.toList . S.fromList $ toList allD8 <&> \d -> map ((+ 1) . orientPoint d . subtract 1) $ S.toList x
-    coord <- toList $ fillBoundingBox (V2 0 (bounds - 3))
-    pure $ S.fromList $ (+ coord) <$> rot
-
--- placeAll
-
--- bfs ::
---   forall n.
---   Ord n =>
---   -- | neighborhood
---   (n -> Set n) ->
---   -- | start
---   n ->
---   -- | target
---   (n -> Bool) ->
---   -- | the shortest path, if it exists
---   Maybe [n]
-
-day12b :: _ :~> _
-day12b =
-  MkSol
-    { sParse = sParse day12a
-    , sShow = show
-    , sSolve =
-        noFail $
-          id
+    , sSolve = noFail \(blocks, areas) ->
+        countTrue (\(bounds, ns) -> product bounds >= sum (zipWith (*) (map S.size blocks) ns)) areas
     }
